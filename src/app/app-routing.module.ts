ad514933b28dcfdb59bb0cfe1f2f58a83301ef92
ING. Criss Sinisterra Rivera TNR <you@example.com>
2020-05-15 20:20:30 -0500

    estante

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { EstudiantesComponent } from './pages/estudiantes/estudiantes.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ClienteComponent } from './pages/cliente/cliente.component';
+import { EstanteComponent } from './pages/estante/estante.component';
+
 
 
 
@@ -13,6 +15,7 @@ const routes: Routes = [
   {path: 'estudiantes', component: EstudiantesComponent},
   {path: 'login', component: LoginComponent},
   {path: 'cliente', component: ClienteComponent},
+  {path: 'estante', component: EstanteComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
